f6fbf966cd9f8ed300ef6fee9d2368413ca4a304
Charles <charlesmelki@gmail.com>
2021-04-23 13:36:15 +0200

    Export type for RootState and AppDispatch

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,17 @@ import rootReducer from "../reducers"
 
 const sagaMiddleware = createSagaMiddleWare()
 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // Remove on production
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeEnhancers(
+    applyMiddleware(sagaMiddleware)
+  )
 )
 
 sagaMiddleware.run(rootSaga)
 
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 export default store
