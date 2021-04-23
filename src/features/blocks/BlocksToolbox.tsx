f3b6fbc1285da5ec63fd8730d1b2f46299989dd2
Charles <charlesmelki@gmail.com>
2021-04-23 10:50:56 +0200

    [FIX] Improved code readability for component with multiple props

--- src/features/blocks/BlocksToolbox.tsx
+++ src/features/blocks/BlocksToolbox.tsx
@@ -6,8 +6,15 @@ function BlocksToolbox({categories}: ToolboxConfiguration) {
   return (
     <div className={'toolbox'}>
       {categories.map(category => {
-        return <BlocksCategory key={category.id} name={category.name} blocks={category.blocks}
-                               highlight={category.highlight} id={category.id}/>
+        return (
+          <BlocksCategory
+            key={category.id}
+            name={category.name}
+            blocks={category.blocks}
+            highlight={category.highlight}
+            id={category.id}
+          />
+        )
       })}
     </div>
   )
