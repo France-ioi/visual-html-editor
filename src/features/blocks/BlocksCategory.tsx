a1d11796a7c7a8375068f09666a8e9efb452d8bb
Charles <charlesmelki@gmail.com>
2021-04-24 11:08:32 +0200

    Updated BlocksCategory component to behave in a more react-friendly way using toggled state prop

--- src/features/blocks/BlocksCategory.tsx
+++ src/features/blocks/BlocksCategory.tsx
@@ -3,8 +3,10 @@ import {ToolboxCategory} from "../../toolboxconfig"
 import Block from "./Block"
 import {toggleCategoryAction} from "../../store/features/blocks/blocksCategory"
 import {useAppDispatch} from "../../hooks"
+import {useRef} from "react";
 
 function BlocksCategory(props: ToolboxCategory) {
+  const categoryBlocksRef = useRef<HTMLDivElement>(null)
   const dispatch = useAppDispatch()
 
   function toggleCategory(categoryId: Number) {
@@ -15,12 +17,23 @@ function BlocksCategory(props: ToolboxCategory) {
     borderLeft: `10px solid ${props.highlight}`
   }
 
+  // Handle max-height property of .toolbox-category-blocks for accordion effect
+  if (props.toggled && categoryBlocksRef.current) {
+    let categoryBlocksInnerHeight = 0
+    categoryBlocksRef.current.childNodes.forEach((block: any) => {
+      categoryBlocksInnerHeight += block.clientHeight
+    })
+    categoryBlocksRef.current.style.maxHeight = `${categoryBlocksInnerHeight}px`
+  } else if (categoryBlocksRef.current) {
+    categoryBlocksRef.current.style.maxHeight = '0'
+  }
+
   return (
     <div className={'toolbox-category'} style={categoryStyle}>
       <span className={'toolbox-category-title'} onClick={() => toggleCategory(props.id)}>
         {props.name}
       </span>
-      <div className={'toolbox-category-blocks'}>
+      <div className={'toolbox-category-blocks'} ref={categoryBlocksRef}>
         {props.blocks.map(block => {
           return <Block key={block.id} tag={block.tag} paired={block.paired} desc={block.desc} id={undefined}/>
         })}
