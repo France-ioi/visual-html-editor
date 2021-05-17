3345c0a698803b796f788d43b8733ac4f711bdbc
Charles Melki <charlesmelki@gmail.com>
2021-05-17 20:55:25 +0200

    Revert "Temp iframe feature commit"
    This reverts commit 28b876355e622f349eebf4790d49a05be76153d0.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import BlocksToolbox from './features/blocks/BlocksToolbox'
 import VisualHTMLEditor from './features/editors/VisualHTMLEditor'
 import {useAppDispatch, useAppSelector} from "./hooks"
 import {
-  DragDropContext,
   DraggableStateSnapshot,
   DraggingStyle,
   DragUpdate,
   DropResult,
   NotDraggingStyle,
 } from "react-beautiful-dnd"
+import {DragDropContext} from "react-beautiful-dnd"
 import {createElement, deleteElement, moveElement} from "./store/features/editors/HTMLEditors"
 import TextualHTMLEditor from "./features/editors/TextualHTMLEditor"
-import {EditorType, TagType} from "./editorconfig"
-import PreviewWebpage from "./store/features/preview/PreviewWebpage";
+import {CodeSegments} from "./editorconfig"
 
 // Used to cancel transition animation for certain draggables
 export function getDragStyle(style: DraggingStyle | NotDraggingStyle | undefined, snapshot: DraggableStateSnapshot) {
@@ -33,22 +32,6 @@ function App() {
   const editorConfig = useAppSelector(state => state.visualHTMLReducer)
   const dispatch = useAppDispatch()
 
-  let codeToInsert = ""
-
-  function refreshPreview() {
-    editorConfig.type === EditorType.Textual ?
-      codeToInsert = editorConfig.codeString
-      :
-      editorConfig.codeElements.map(e => {
-        let tag = ''
-        if (e.type === TagType.Opening) tag = '<' + e.value + '>'
-        else if (e.type === TagType.Closing) tag = '</' + e.value + '>'
-        else tag = `${e.value} `
-        return codeToInsert += tag
-      })
-    return codeToInsert
-  }
-
   const onDragEnd = (result: DropResult) => {
     if (result.source && result.destination) {
       if (result.destination.droppableId === 'toolbox-dropzone' && result.source.droppableId !== 'toolbox-dropzone') {
@@ -65,8 +48,6 @@ function App() {
   const onDragUpdate = (update: DragUpdate) => {
   }
 
-  refreshPreview()
-
   return (
     <div className="App">
       <DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
@@ -77,7 +58,6 @@ function App() {
             :
             <TextualHTMLEditor elements={editorConfig.codeString}/>
         }
-        <PreviewWebpage code={codeToInsert}/>
       </DragDropContext>
     </div>
   )
