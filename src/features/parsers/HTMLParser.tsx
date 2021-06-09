6fa972cc18934d1d1f2d506adb1f1146eae68b25
Charles <charlesmelki@gmail.com>
2021-06-09 18:12:56 +0200

    Change file type to tsx

--- src/features/parsers/HTMLParser.tsx
+++ src/features/parsers/HTMLParser.tsx
@@ -6,6 +6,10 @@ function HTMLParser() {
   const editorMode = useAppSelector(state => state.visualHTMLReducer.type)
   const visualHTMLCode = useAppSelector(state => state.visualHTMLReducer.codeElements)
   const textualHTMLCode = useAppSelector(state => state.visualHTMLReducer.codeString)
+
+  return (
+    <></>
+  )
 }
 
 export default HTMLParser
