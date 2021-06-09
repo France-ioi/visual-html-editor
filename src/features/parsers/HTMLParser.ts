1942fb22f5945367548d88bff18b2bade5387424
Charles Melki <charlesmelki@gmail.com>
2021-06-09 18:04:04 +0200

    New HTML parser component

---
+++ src/features/parsers/HTMLParser.ts
@@ -0,0 +1,11 @@
+import {useAppSelector} from "../../hooks";
+
+let tinyhtml = require('tiny-html-lexer')
+
+function HTMLParser() {
+  const editorMode = useAppSelector(state => state.visualHTMLReducer.type)
+  const visualHTMLCode = useAppSelector(state => state.visualHTMLReducer.codeElements)
+  const textualHTMLCode = useAppSelector(state => state.visualHTMLReducer.codeString)
+}
+
+export default HTMLParser
